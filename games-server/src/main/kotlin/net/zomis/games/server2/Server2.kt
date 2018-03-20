da0f603657f7d3b6a3326adbb2192bef4aa95178
Simon Forsberg <zomis2k@hotmail.com>
2018-03-20 23:55:46 +0100

    Add Connect4 game implementation to make players actually able to play

--- games-server/src/main/kotlin/net/zomis/games/server2/Server2.kt
+++ games-server/src/main/kotlin/net/zomis/games/server2/Server2.kt
@@ -6,6 +6,7 @@ import klogging.KLoggers
 import net.zomis.core.events.EventSystem
 import net.zomis.games.server2.games.GameSystem
 import net.zomis.games.server2.games.SimpleMatchMakingSystem
+import net.zomis.games.server2.games.impl.Connect4
 import net.zomis.games.server2.ws.Server2WS
 import java.net.InetSocketAddress
 
@@ -30,6 +31,9 @@ class Server2(val port: Int) {
             }
         })
         val gameSystem = GameSystem(events)
+
+        Connect4.init(events)
+
         SimpleMatchMakingSystem(gameSystem, events)
 
         events.execute(StartupEvent())
---
+++ games-server/src/main/kotlin/net/zomis/games/server2/games/impl/Connect4.kt
@@ -0,0 +1,64 @@
+package net.zomis.games.server2.games.impl
+
+import com.fasterxml.jackson.databind.node.IntNode
+import klogging.KLoggers
+import net.zomis.core.events.EventSystem
+import net.zomis.games.server2.games.*
+import net.zomis.tttultimate.TTFactories
+import net.zomis.tttultimate.TTPlayer
+import net.zomis.tttultimate.games.TTClassicControllerWithGravity
+
+fun TTPlayer.playerIndex(): Int {
+    if (this == TTPlayer.X) return 0
+    if (this == TTPlayer.O) return 1
+    throw IllegalArgumentException("Current player must be X or O but was $this")
+}
+
+class Connect4 {
+
+    companion object {
+        private val logger = KLoggers.logger(this)
+        fun init(events: EventSystem) {
+            events.addListener(PlayerGameMoveRequest::class, {
+                if (it.game.gameType.type == "Connect4") {
+                    if (it.game.obj == null) {
+                        it.game.obj = TTClassicControllerWithGravity(TTFactories().classicMNK(7, 6, 4))
+                    }
+                    val x = (it.move as IntNode).intValue()
+                    val controller = it.game.obj as TTClassicControllerWithGravity
+                    if (controller.isGameOver) {
+                        events.execute(it.illegalMove("Game already won by ${controller.wonBy}"))
+                        return@addListener
+                    }
+                    if (controller.currentPlayer.playerIndex() == it.player) {
+                        val playAt = (0 until controller.game.sizeY).asSequence()
+                            .map { controller.game.getSub(x, it) }
+                            .filter { !it.isWon }
+                            .lastOrNull()
+
+                        logger.info { "${it.game} Player ${it.player} played at $x" }
+                        if (playAt != null && controller.play(playAt)) {
+                            events.execute(MoveEvent(it.game, it.player, x))
+                        } else {
+                            events.execute(it.illegalMove("Not allowed to play there"))
+                        }
+
+                        if (controller.isGameOver) {
+                            val winner = controller.wonBy
+                            it.game.players.indices.forEach({ playerIndex ->
+                                val won = winner.playerIndex() == playerIndex
+                                val losePositionPenalty = if (won) 0 else 1
+                                events.execute(PlayerEliminatedEvent(it.game, playerIndex, won, 1 + losePositionPenalty))
+                            })
+                            events.execute(GameEndedEvent(it.game))
+                        }
+                    } else {
+                        events.execute(it.illegalMove("Not your turn"))
+                    }
+                }
+            })
+            events.execute(GameTypeRegisterEvent("Connect4"))
+        }
+    }
+
+}
