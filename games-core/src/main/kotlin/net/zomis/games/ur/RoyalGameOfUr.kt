95964c8b4cf427a8cdf09abe42f709e2d6669477
Simon Forsberg <zomis2k@hotmail.com>
2018-03-22 00:23:46 +0100

    Add RoyalGameOfUr

---
+++ games-core/src/main/kotlin/net/zomis/games/ur/RoyalGameOfUr.kt
@@ -0,0 +1,265 @@
+package net.zomis.games.ur
+
+/*
+// 0 = Outside
+// 4 = First flower
+Fxxx  Fx
+xxxFxxxx
+Fxxx  Fx
+*/
+class RoyalGameOfUr {
+
+    constructor() {
+        this.currentPlayer = 0
+        this.roll = NOT_ROLLED
+        this.pieces = Array(2) { IntArray(7) }
+    }
+
+    constructor(currentPlayer: Int, roll: Int, pieces: Array<IntArray>) {
+        this.currentPlayer = currentPlayer
+        this.roll = roll
+        this.pieces = pieces.map { it.copyOf() }.toTypedArray()
+    }
+
+    var currentPlayer: Int private set
+
+    var roll: Int = NOT_ROLLED
+        private set
+
+    private val pieces: Array<IntArray>
+
+    // pieces[PLAYER][PIECE]
+
+    fun isRollTime(): Boolean {
+        return roll == NOT_ROLLED
+    }
+
+    val isMoveTime: Boolean
+        get() = roll > 0
+
+    val piecesCopy: Array<IntArray>
+        get() = this.pieces.map({ arr -> arr.copyOf() }).toTypedArray()
+
+    val isFinished: Boolean
+        get() = winner != NO_WINNER
+
+    val winner: Int
+        get() {
+            for (i in this.pieces.indices) {
+                if (this.pieces[i].all { v -> v == EXIT }) {
+                    return i
+                }
+            }
+            return NO_WINNER
+        }
+
+    fun randomBoolean(): Boolean {
+        return (0..1).shuffled().first() == 1
+    }
+
+    fun roll(): Int {
+        if (!isRollTime()) {
+            throw IllegalStateException("Not time to roll. Current roll is $roll")
+        }
+
+        val sum = (0..3).map { if (randomBoolean()) 1 else 0 }.sum()
+        if (canMove(sum)) {
+            this.roll = sum
+        } else {
+            this.nextPlayer()
+        }
+        return sum
+    }
+
+    private fun nextPlayer() {
+        this.currentPlayer = (this.currentPlayer + 1) % 2
+    }
+
+    fun canMove(roll: Int): Boolean {
+        if (roll == 0) {
+            return false
+        }
+        // Loop through player's pieces and check if they can move `this.roll` steps.
+        for (i in 0 until this.pieces[currentPlayer].size) {
+            val position = this.pieces[currentPlayer][i]
+            val nextPosition = position + roll
+
+            if (canMoveTo(currentPlayer, nextPosition)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun canMoveTo(currentPlayer: Int, nextPosition: Int): Boolean {
+        if (isFinished) {
+            return false
+        }
+        if (nextPosition == EXIT) {
+            return true
+        }
+        if (nextPosition > EXIT) {
+            return false
+        }
+        if (nextPosition >= 5 && nextPosition <= EXIT - 3) {
+            // Shared area. Can knock out except on flower.
+            if (isFlower(nextPosition)) {
+                for (player in this.pieces.indices) {
+                    if (playerOccupies(player, nextPosition)) {
+                        return false
+                    }
+                }
+                return true
+            } else {
+                return !playerOccupies(currentPlayer, nextPosition)
+            }
+        } else {
+            // Private area
+            return !playerOccupies(currentPlayer, nextPosition)
+        }
+    }
+
+    fun playerOccupies(currentPlayer: Int, position: Int): Boolean {
+        for (piece in 0 until this.pieces[currentPlayer].size) {
+            if (this.pieces[currentPlayer][piece] == position) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun canMove(playerIndex: Int, position: Int, steps: Int): Boolean {
+        return playerOccupies(playerIndex, position) && canMoveTo(playerIndex, position + steps)
+    }
+
+    fun move(playerIndex: Int, position: Int, steps: Int): Boolean {
+        if (isFinished) {
+            return false
+        }
+        if (!canMove(playerIndex, position, steps)) {
+            return false
+        }
+        for (i in 0 until this.pieces[playerIndex].size) {
+            if (this.pieces[playerIndex][i] == position) {
+                this.pieces[playerIndex][i] = position + steps
+                performKnockout(playerIndex, position + steps)
+                this.roll = NOT_ROLLED
+                if (!isFlower(position + steps)) {
+                    this.nextPlayer()
+                }
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun performKnockout(playerIndex: Int, position: Int) {
+        val opponent = (playerIndex + 1) % this.pieces.size
+        if (position <= 4 || position > 12) {
+            return
+        }
+        if (isFlower(position)) {
+            return
+        }
+        for (j in 0 until this.pieces[opponent].size) {
+            if (this.pieces[opponent][j] == position) {
+                this.pieces[opponent][j] = 0
+            }
+        }
+    }
+
+    fun isFlower(position: Int): Boolean {
+        return position == 4 || position == 8 || position == EXIT - 1
+    }
+
+    fun canKnockout(position: Int): Boolean {
+        return position > 4 && position < EXIT - 2 && !isFlower(position)
+    }
+
+    override fun toString(): String {
+        return "RoyalGameOfUr{" +
+                "pieces=" + pieces.contentDeepToString() +
+                ", currentPlayer=" + currentPlayer +
+                ", roll=" + roll +
+                '}'.toString()
+    }
+
+    fun toLong(): Long {
+        val cp = currentPlayer
+        val op = 1 - cp
+
+        var result: Long = 0
+        val numberHome1 = pieces[cp].filter({ i -> i == 0 }).count()
+        val numberHome2 = pieces[op].filter({ i -> i == 0 }).count()
+        val numberGoal1 = pieces[cp].filter({ i -> i == EXIT }).count()
+        val numberGoal2 = pieces[op].filter({ i -> i == EXIT }).count()
+        var dice = roll - 1 // 1..4 --> 0..3
+        if (isFinished) {
+            dice = 0
+        } else if (dice < 0 || dice >= 4) {
+            throw IllegalStateException("Invalid dice value for serializing: $dice")
+        }
+        val p1 = piecesToArray(pieces[cp])
+        val p2 = piecesToArray(pieces[op])
+
+        result += numberHome1.toLong()
+        result = result shl 3
+        result += numberHome2.toLong()
+        result = result shl 3
+        result += numberGoal1.toLong()
+        result = result shl 3
+        result += numberGoal2.toLong()
+        result = result shl 3
+
+        result += dice.toLong()
+        result = result shl 2
+        for (b in p1) {
+            result += (if (b) 1 else 0).toLong()
+            result = result shl 1
+        }
+        for (b in p2) {
+            result += (if (b) 1 else 0).toLong()
+            result = result shl 1
+        }
+        return result
+    }
+
+    private fun piecesToArray(pieces: IntArray): BooleanArray {
+        val result = BooleanArray(14)
+        for (p in pieces) {
+            if (p != 0 && p != 15) {
+                result[p - 1] = true
+            }
+        }
+        return result
+    }
+
+    fun toCompactString(): String {
+        val str = StringBuilder()
+        str.append(currentPlayer)
+        val p0 = pieces[0].copyOf()
+        p0.sort()
+
+        val p1 = pieces[1].copyOf()
+        p1.sort()
+
+        for (aP0 in p0) {
+            str.append(aP0)
+        }
+        for (aP1 in p1) {
+            str.append(aP1)
+        }
+        return str.toString()
+    }
+
+    fun copy(): RoyalGameOfUr {
+        return RoyalGameOfUr(this.currentPlayer, this.roll, piecesCopy)
+    }
+
+    companion object {
+        private val NOT_ROLLED = -1
+        private val NO_WINNER = -1
+        val EXIT = 15
+    }
+
+}
---
+++ games-server/src/main/kotlin/net/zomis/games/server2/games/impl/RoyalGameOfUrSystem.kt
@@ -0,0 +1,67 @@
+package net.zomis.games.server2.games.impl
+
+import com.fasterxml.jackson.databind.node.IntNode
+import klogging.KLoggers
+import net.zomis.core.events.EventSystem
+import net.zomis.games.server2.games.*
+import net.zomis.games.ur.RoyalGameOfUr
+import net.zomis.tttultimate.games.TTClassicControllerWithGravity
+
+class RoyalGameOfUrSystem {
+
+    companion object {
+        private val logger = KLoggers.logger(this)
+        fun init(events: EventSystem) {
+            events.addListener(PlayerGameMoveRequest::class, {
+                if (it.game.gameType.type != "UR") {
+                    return@addListener
+                }
+                if (it.game.obj == null) {
+                    it.game.obj = RoyalGameOfUr()
+                }
+                val controller = it.game.obj as RoyalGameOfUr
+                if (controller.isFinished) {
+                    events.execute(it.illegalMove("Game already won by ${controller.winner}"))
+                    return@addListener
+                }
+
+                val oldPlayer = controller.currentPlayer
+                if (it.moveType == "roll") {
+                    controller.roll()
+                    events.execute(MoveEvent(it.game, it.player, "roll", ""))
+                    events.execute(GameStateEvent(it.game, listOf(Pair("roll", controller.roll))))
+                } else {
+                    val x = (it.move as IntNode).intValue()
+                    if (controller.currentPlayer != it.player) {
+                        events.execute(it.illegalMove("Not your turn"))
+                        return@addListener
+                    }
+                    if (controller.isMoveTime && controller.canMove(x)) {
+                        controller.move(controller.currentPlayer, x, controller.roll)
+                        logger.info { "${it.game} Player ${it.player} made move $x" }
+                    } else {
+                        events.execute(it.illegalMove("Not allowed to play there"))
+                    }
+                    events.execute(MoveEvent(it.game, it.player, "move", x))
+                }
+                if (controller.currentPlayer != oldPlayer) {
+                    events.execute(GameStateEvent(it.game, listOf(Pair("player", controller.currentPlayer))))
+                }
+
+
+                if (controller.isFinished) {
+                    val winner = controller.winner
+                    it.game.players.indices.forEach({ playerIndex ->
+                        val won = winner == playerIndex
+                        val losePositionPenalty = if (won) 0 else 1
+                        events.execute(PlayerEliminatedEvent(it.game, playerIndex, won, 1 + losePositionPenalty))
+                    })
+                    events.execute(GameEndedEvent(it.game))
+                }
+            })
+            events.execute(GameTypeRegisterEvent("UR"))
+        }
+    }
+
+
+}
