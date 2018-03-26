6a23d27cd317a14e5161d9668f1d762e2c60a645
Simon Forsberg <zomis2k@hotmail.com>
2018-03-26 21:39:31 +0200

    Add POC RoyalGameOfUr code to StartScreen - play 40 moves

--- games-vue-client/src/components/StartScreen.vue
+++ games-vue-client/src/components/StartScreen.vue
@@ -8,6 +8,25 @@
 </template>
 
 <script>
+let games = require("../../../games-js/web/games-js");
+let ur = new games.net.zomis.games.ur.RoyalGameOfUr_init();
+console.log(ur.toString());
+
+for (var i = 0; i < 40; i++) {
+  ur.doRoll();
+  if (ur.isMoveTime) {
+    var player = ur.currentPlayer;
+    var roll = ur.roll;
+    for (var m = 0; m < 15; m++) {
+      if (ur.move_qt1dr2$(ur.currentPlayer, m, roll)) {
+        console.log(player + " made move " + m + " for roll " + roll);
+        break;
+      }
+    }
+  }
+}
+console.log(ur.toString());
+
 import Socket from "../socket";
 export default {
   name: "StartScreen",
