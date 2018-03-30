15a7bc51920e89646f13d6d85ff2b08c76176c50
Simon Forsberg <zomis2k@hotmail.com>
2018-03-30 16:22:03 +0200

    Client: Add some prettier dice

--- games-vue-client/src/components/ur/UrRoll.vue
+++ games-vue-client/src/components/ur/UrRoll.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="ur-roll">
+    <div class="ur-dice">
+      <div v-for="i in 4" class="ur-die">
+        <div v-if="roll >= i" class="ur-die-filled"></div>
+      </div>
+    </div>
     <span>{{ roll }}</span>
     <button :disabled="!usable" @click="onclick()">Roll</button>
   </div>
@@ -11,7 +16,6 @@ export default {
   props: ["roll", "usable", "onDoRoll"],
   methods: {
     onclick: function() {
-      console.log("UrRoll onclick");
       this.onDoRoll();
     }
   }
@@ -20,6 +24,29 @@ export default {
 <style scoped>
 .ur-roll {
   margin-top: 10px;
+}
 
+.ur-dice {
+  width: 320px;
+  height: 64px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.ur-die-filled {
+  background: black;
+  border-radius: 100%;
+  width: 20%;
+  height: 20%;
+}
+
+.ur-die {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  border: 1px solid black;
+  border-radius: 12px;
 }
 </style>
