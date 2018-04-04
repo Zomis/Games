1059fabd14107583dfad91986290fd7a3a0b84db
Simon Forsberg <zomis2k@hotmail.com>
2018-04-04 21:32:09 +0200

    Fix a test

--- games-server/src/test/kotlin/net/zomis/games/server2/URIntegrationTest.kt
+++ games-server/src/test/kotlin/net/zomis/games/server2/URIntegrationTest.kt
@@ -3,11 +3,11 @@ package net.zomis.games.server2
 import net.zomis.games.server2.clients.ur.RandomUrBot
 
 fun main(args: Array<String>) {
-    val server2 = Server2(8389)
-    server2.start(arrayOf())
+    val server2 = Server2()
+    server2.start(testServerConfig())
     println(server2)
     Thread({ RandomUrBot("ws://127.0.0.1:8389").play() }).start()
     Thread({ RandomUrBot("ws://127.0.0.1:8389").play() }).start()
 
-    Thread.sleep(2000)
+    Thread.sleep(2000) // If this is not here, main will die before new threads start
 }
