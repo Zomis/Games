50be00cb64d9593fd7db2738e3fae565179200cb
Simon Forsberg <zomis2k@hotmail.com>
2018-04-07 18:11:40 +0200

    Improvements to EventSystem

--- games-core/src/main/kotlin/net/zomis/core/events/EventSystem.kt
+++ games-core/src/main/kotlin/net/zomis/core/events/EventSystem.kt
@@ -5,19 +5,31 @@ import kotlin.reflect.KClass
 
 enum class ListenerPriority { FIRST, EARLIER, EARLY, NORMAL, LATE, LATER, LAST }
 
+class Listener<in E>(val description: String, val condition: (E) -> Boolean, val handler: EventHandler<E>) {
+
+    private val logger = KLoggers.logger(this)
+
+    fun run(event: E) {
+        if (condition.invoke(event)) {
+            logger.info { "Running '$description' for event $event" }
+            handler.invoke(event)
+        }
+    }
+}
+
 class ListenerList<E> {
 
     private val logger = KLoggers.logger(this)
-    private val list: MutableList<EventHandler<E>> = mutableListOf()
+    private val list: MutableList<Listener<E>> = mutableListOf()
 
-    fun add(eventHandler: EventHandler<E>) {
+    fun add(eventHandler: Listener<E>) {
         list.add(eventHandler)
     }
 
     fun execute(event: E) {
         list.forEach({
             try {
-                it.invoke(event)
+                it.run(event)
             } catch (e: RuntimeException) {
                 logger.error(e, "Problem when handling event $event in $it")
                 throw e
@@ -32,15 +44,20 @@ class EventSystem {
     private val logger = KLoggers.logger(this)
     private val listeners: MutableMap<KClass<Any>, ListenerList<Any>> = HashMap()
 
-    fun <E : Any> addListener(clazz: KClass<E>, handler: EventHandler<E>) {
-        addListener(clazz, handler, ListenerPriority.NORMAL)
-    }
-
-    fun <E : Any> addListener(clazz: KClass<E>, handler: EventHandler<E>, priority: ListenerPriority) {
+    fun <E : Any> listen(description: String, priority: ListenerPriority, clazz: KClass<E>,
+         condition: (E) -> Boolean, handler: EventHandler<E>) {
         logger.info("Add Listener with priority $priority to $clazz: $handler")
         val list: ListenerList<E> = listeners.getOrElse(clazz as KClass<Any>, { ListenerList<E>() }) as ListenerList<E>
-        list.add(handler)
+        list.add(Listener(description, condition, handler))
         listeners[clazz] = list as ListenerList<Any>
+    }
+
+    fun <E : Any> listen(description: String, clazz: KClass<E>, condition: (E) -> Boolean, handler: EventHandler<E>) {
+        return listen(description, ListenerPriority.NORMAL, clazz, condition, handler)
+    }
+
+    fun <E : Any> addListener(clazz: KClass<E>, handler: EventHandler<E>) {
+        return listen(handler.toString(), ListenerPriority.NORMAL, clazz, {true}, handler)
     }
 
     fun <E : Any> execute(event: E) {
@@ -49,8 +66,9 @@ class EventSystem {
         listeners[kclass]?.execute(event)
     }
 
-    fun with(registrator: EventRegistrator) {
+    fun with(registrator: EventRegistrator): EventSystem {
         registrator.invoke(this)
+        return this
     }
 
 }
