cc8f0437d9ab81f69b5091148cfab6c9b793c509
Simon Forsberg <zomis2k@hotmail.com>
2018-04-14 01:11:57 +0200

    Add a back to lobby button in GameResult component

--- games-vue-client/src/components/games/common/GameResult.vue
+++ games-vue-client/src/components/games/common/GameResult.vue
@@ -4,7 +4,7 @@
     <span v-if="gameOverMessage.winner" class="win">WIN</span>
     <span v-if="!gameOverMessage.winner" class="loss">LOSE</span>
     <div class="game-over-actions">
-      <!-- TODO: Back to lobby, rematch, etc. -->
+      <router-link to="/connected">Back to Lobby</router-link>
     </div>
   </div>
 </template>
