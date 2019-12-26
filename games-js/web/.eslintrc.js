2f940160841e5fde64883b6d8ed84d881de49bee
Simon Forsberg <zomis2k@hotmail.com>
2019-12-26 18:53:11 +0100

    Add eslintrc for games-js/web directory

---
+++ games-js/web/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-func-assign': 'warn',
+    'no-constant-condition': 'warn',
+    'no-unused-vars': 'off',
+    'no-undef': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+}
