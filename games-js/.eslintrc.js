a78bf77a1872d5540a3d65239bbb0b8ccdf9c00f
Simon Forsberg <zomis2k@hotmail.com>
2019-12-26 19:16:25 +0100

    Fix eslint configuration

--- games-js/.eslintrc.js
+++ games-js/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
     'no-func-assign': 'warn',
     'no-constant-condition': 'warn',
     'no-unused-vars': 'off',
+    'no-unused-labels': 'warn',
+    'no-self-assign': 'warn',
     'no-undef': 'off'
   },
   parserOptions: {
