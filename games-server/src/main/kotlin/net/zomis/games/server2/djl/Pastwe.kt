261a7e742a644743556a1125f48cf36f124f15d7
Simon Forsberg <zomis2k@hotmail.com>
2020-03-28 20:51:18 +0100

    Some training progress (at least no compiler errors)

--- games-server/src/main/kotlin/net/zomis/games/server2/djl/Pastwe.kt
+++ games-server/src/main/kotlin/net/zomis/games/server2/djl/Pastwe.kt
@@ -2,10 +2,14 @@ package net.zomis.games.server2.djl
 
 import ai.djl.Model
 import ai.djl.basicmodelzoo.basic.Mlp
+import ai.djl.ndarray.NDArray
 import ai.djl.ndarray.NDList
-import ai.djl.ndarray.types.DataType
+import ai.djl.ndarray.NDManager
 import ai.djl.ndarray.types.Shape
 import ai.djl.training.DefaultTrainingConfig
+import ai.djl.training.dataset.ArrayDataset
+import ai.djl.training.dataset.Batch
+import ai.djl.training.dataset.SequenceSampler
 import ai.djl.training.loss.Loss
 import ai.djl.translate.Translator
 import ai.djl.translate.TranslatorContext
@@ -13,8 +17,13 @@ import kotlin.random.Random
 
 class DJLReinforcementMCVE2 {
 
-    class HelloWorldExperience(val state: BooleanArray, val action: Int, val reward: Float)
-    // TODO: total reward, next state?
+    class HelloWorldExperience(
+        val state: BooleanArray,
+        val action: Int,
+        val reward: Float,
+        val nextState: BooleanArray,
+        val gameOver: Boolean
+    )
     class HelloWorldGame(val size: Int) {
         val values: BooleanArray = (0 until size).map { false }.toBooleanArray()
 
@@ -29,8 +38,20 @@ class DJLReinforcementMCVE2 {
         }
     }
 
+    class HelloWorldNoTranslation : Translator<BooleanArray, FloatArray> {
+        override fun processOutput(ctx: TranslatorContext, list: NDList): FloatArray {
+            return list[0].toFloatArray()
+        }
+
+        override fun processInput(ctx: TranslatorContext, input: BooleanArray): NDList {
+            return NDList(ctx.ndManager.create(input.map { if (it) 1f else 0f }.toFloatArray()))
+        }
+
+    }
+
     class HelloWorldGameTranslator : Translator<BooleanArray, Int> {
         private val random = Random(42)
+        private val inputTranslator = HelloWorldNoTranslation()
 
         override fun processOutput(ctx: TranslatorContext, list: NDList): Int {
             val values = list[0].toFloatArray()
@@ -40,9 +61,8 @@ class DJLReinforcementMCVE2 {
             return values.withIndex().maxBy { it.value.toDouble() }!!.index
         }
 
-        override fun processInput(ctx: TranslatorContext, input: BooleanArray): NDList {
-            return NDList(ctx.ndManager.create(input.map { if (it) 1f else 0f }.toFloatArray()))
-        }
+        override fun processInput(ctx: TranslatorContext, input: BooleanArray): NDList
+            = inputTranslator.processInput(ctx, input)
     }
 
     class Agent {
@@ -59,37 +79,73 @@ class DJLReinforcementMCVE2 {
         }
 
         fun decideMoveIndex(state: BooleanArray): Int = predictor.predict(state)
-        fun saveExperience(state: BooleanArray, moveIndex: Int, reward: Float) {
-            experiences.add(HelloWorldExperience(state, moveIndex, reward))
+        fun saveExperience(state: BooleanArray, moveIndex: Int, reward: Float,
+                       nextState: BooleanArray, gameOver: Boolean) {
+            experiences.add(HelloWorldExperience(state, moveIndex, reward, nextState, gameOver))
         }
         fun train() {
-            val randomXP = experiences.shuffled().take(5)
+            val batchSize = 5
+            val batch = experiences.shuffled().take(batchSize)
+            val trainTranslator = HelloWorldNoTranslation()
+            val trainPredictor = model.newPredictor(trainTranslator)
+            val qTarget = trainPredictor.batchPredict(batch.map { it.nextState })
 
-//            trainer.iterateDataset()
-//            Batch()
-//            trainer.trainBatch()
-//            trainer.step()
-//            trainer.
+            batch.withIndex().filter { it.value.gameOver }.forEach {
+                // Terminal states don't really have a qValue so ignore them from next state
+                model.ndManager.create(floatArrayOf(0f, 0f, 0f, 0f))
+            }
+
+            val dataset = ArrayDataset.Builder()
+                .setData(*batch.map { it.state.toNDArray(model.ndManager) }.toTypedArray())
+                .optLabels(*qTarget.withIndex().map {
+                    // What about the action taken?
+                    model.ndManager.create(it.value).mul(0.9).add(batch[it.index].reward)
+                }.toTypedArray())
+                .setSampling(batchSize, false)
+                .build()
+            // optimizer learning rate?
+
+//            Batch(model.ndManager, data, labels)
+            trainer.iterateDataset(dataset).forEach {
+                trainer.trainBatch(it)
+                trainer.step()
+            }
+
+
+//            _, cost = sess.run([qnn.optimizer, qnn.cost],
+//            feed_dict={qnn.states: np.array(list(map(lambda x: x['state'], batch))),
+//                qnn.r: np.array(list(map(lambda x: x['reward'], batch))),
+//                qnn.enum_actions: np.array(list(enumerate(map(lambda x: x['action'], batch)))),
+//                qnn.q_target: q_target})
         }
     }
 
     fun run() {
         val agent = Agent()
         var actionsMade = 0
-        repeat(10) {
+        repeat(10) {gameNumber ->
             val game = HelloWorldGame(4)
+            var totalReward = 0f
             while (!game.isDone()) {
                 val state = game.values.copyOf()
                 val moveIndex = agent.decideMoveIndex(state)
                 val reward = game.performActionObserveReward(moveIndex)
-                agent.saveExperience(state, moveIndex, reward)
+                totalReward += reward
+                val nextState = game.values.copyOf()
+                agent.saveExperience(state, moveIndex, reward, nextState, game.isDone())
                 if (++actionsMade % 10 == 0) {
+                    println("$actionsMade actions made. Training time")
                     agent.train()
                 }
             }
+            println("Finished game $gameNumber with $totalReward")
         }
     }
 
+}
+
+private fun BooleanArray.toNDArray(manager: NDManager): NDArray {
+    return manager.create(this.map { if (it) 1f else 0f }.toFloatArray())
 }
 
 fun main() {
