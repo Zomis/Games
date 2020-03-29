da732127184eee23db475add45def8ece305db4a
Simon Forsberg <zomis2k@hotmail.com>
2020-03-29 12:30:08 +0200

    Apply learnt code to generic style

--- games-server/src/main/kotlin/net/zomis/games/server2/djl/DJLReinforcement.kt
+++ games-server/src/main/kotlin/net/zomis/games/server2/djl/DJLReinforcement.kt
@@ -5,9 +5,13 @@ import ai.djl.basicmodelzoo.basic.Mlp
 import ai.djl.ndarray.NDArray
 import ai.djl.ndarray.NDList
 import ai.djl.ndarray.NDManager
+import ai.djl.ndarray.types.Shape
 import ai.djl.training.DefaultTrainingConfig
+import ai.djl.training.dataset.ArrayDataset
 import ai.djl.training.initializer.XavierInitializer
 import ai.djl.training.loss.Loss
+import ai.djl.training.optimizer.Optimizer
+import ai.djl.training.optimizer.learningrate.LearningRateTracker
 import ai.djl.translate.Translator
 import ai.djl.translate.TranslatorContext
 import net.zomis.bestBy
@@ -18,103 +22,149 @@ import kotlin.random.Random
 
 class DJLReinforcement {
 
-    data class TranslatedView(val floats: NDArray)
-    data class Experience(val view: TranslatedView, val action: Int, val reward: Double)
+    class LearningExperience(
+        val state: FloatArray,
+        val action: Int,
+        val reward: Float,
+        val nextState: FloatArray,
+        val gameOver: Boolean
+    ) {
+        override fun toString(): String = "${state.joinToString()} action $action. Reward $reward. Next: ${nextState.joinToString()} GameOver $gameOver"
+    }
 
-    class MyTranslator(private val random: Random) : Translator<TranslatedView, Int> {
-        override fun processInput(ctx: TranslatorContext, input: TranslatedView): NDList {
-            return NDList(input.floats)
+    class NoTranslation : Translator<FloatArray, FloatArray> {
+        override fun processInput(ctx: TranslatorContext, input: FloatArray): NDList {
+            return NDList(ctx.ndManager.create(input))
         }
 
-        override fun processOutput(ctx: TranslatorContext, list: NDList): Int {
-            val values = list[0].toFloatArray()
-            if (random.nextDouble() < 0.1) {
-                return random.nextInt(values.size)
-            }
-            return values.withIndex().bestBy { it.value.toDouble() }.random().index
+        override fun processOutput(ctx: TranslatorContext, list: NDList): FloatArray {
+            return list[0].toFloatArray()
         }
     }
 
-    class Agent(random: Random) {
-        private val block = Mlp(4, 4, intArrayOf(4))
+    class LearningAgent(val actions: Int) {
+        private val experiences = mutableListOf<LearningExperience>()
+
+        private val block = Mlp(actions, actions, intArrayOf())
         private val model = Model.newInstance().also {
-            block.setInitializer(XavierInitializer())
             it.block = block
         }
-        private val manager = NDManager.newBaseManager()
-        private val predictor = model.newPredictor(MyTranslator(random))
-
+        private val predictor = model.newPredictor(NoTranslation())
+        private val trainingConfig = DefaultTrainingConfig(Loss.l2Loss()).setBatchSize(2)
+                .optInitializer(XavierInitializer())
+                .optOptimizer(Optimizer.adam().optLearningRateTracker(
+                    LearningRateTracker.fixedLearningRate(0.1f)
+                ).build())
+        private val trainer = model.newTrainer(trainingConfig)
         init {
-            val trainingConfig = DefaultTrainingConfig(Loss.l2Loss())
-//                .addEvaluator(Accuracy())
-                .setBatchSize(2)
-//            val inputShape = ai.djl.ndarray.types.Shape(1, 4).
-
-//            model.newTrainer(trainingConfig).use { it.initialize(inputShape) }
+            trainer.initialize(Shape(1, actions.toLong()))
         }
 
-        fun understandView(view: Map<String, Any?>): TranslatedView {
-            val v = view["board"] as List<Boolean>
-            return TranslatedView(manager.create(v.map { if (it) 1 else 0 }.toIntArray()))
+        fun runNetwork(state: FloatArray): FloatArray = predictor.predict(state)
+        fun saveExperience(state: FloatArray, moveIndex: Int, reward: Float,
+                   nextState: FloatArray, gameOver: Boolean) {
+            experiences.add(LearningExperience(state, moveIndex, reward, nextState, gameOver))
         }
+        fun train() {
+            val batchSize = 5
+            val batch = experiences.withIndex().shuffled().take(batchSize).also {
+                list -> list.forEach { debug(it.toString()) }
+            }.map { it.value }
+            experiences.clear() // TODO: Use cyclic memory, don't remove all of it.
+            val trainTranslator = NoTranslation()
+            val trainPredictor = model.newPredictor(trainTranslator)
+            val qTarget = trainPredictor.batchPredict(batch.map { it.nextState })
 
-        fun decideMoveIndex(translatedView: TranslatedView): Int {
-            return predictor.predict(translatedView)
+            batch.withIndex().filter { it.value.gameOver }.forEach {
+                // Terminal states don't really have a qValue so ignore them from next state
+                model.ndManager.create((0 until actions).map { 0f }.toFloatArray())
+            }
+
+            val data = model.ndManager.create(batch.map { it.state }.toTypedArray())
+            val labels = model.ndManager.create(qTarget.withIndex().map {
+                // What about the action taken?
+                debug("Label before modify ${it.index}: ${it.value.joinToString()}")
+                val copy = it.value.copyOf()
+                val batchValue = batch[it.index]
+                copy[batchValue.action] = batchValue.reward + 0.5f * copy[batchValue.action]
+                copy
+//                it.value.map { f -> f * 0.9f + batch[it.index].reward }.toFloatArray()
+            }.toTypedArray())
+            debug("Data: $data")
+            debug("Labels: $labels")
+
+            val dataset = ArrayDataset.Builder()
+                .setData(data)
+                .optLabels(labels)
+                .setSampling(batchSize, false)
+                .build()
+
+            trainer.iterateDataset(dataset).forEach {
+                trainer.trainBatch(it)
+                trainer.step()
+            }
         }
-
-        fun observeReward(view: Map<String, Any?>, view2: Map<String, Any?>): Double {
-            val scores = view["score"] as List<Int>
-            val scores2 = view2["score"] as List<Int>
-            return (scores2[0] - scores[0]).toDouble()
-        }
-
-        fun saveExperience(translatedView: TranslatedView, move: Int, reward: Double) {
-            Experience(translatedView, move, reward)
-        }
-
-        fun train(trainingData: Int) {
-            val trainingData = listOf(0).shuffled().take(3)
-        }
-
     }
 
+
     fun play() {
-        // Environments: HelloWorld, Grid-World, TTT to start with.
-        // Later: Reversi, UTTT, Connect4, Quixo, TTT3D
-        // Trickier: Artax (up to 4 players)
-        // Trickier: UR (randomness, next player can vary)
-
-        // Train using a memory of 100 or so experiences
-        // Record state, action and next state(?)
-
-        // Network: Input state, output Q-values for all actions
-
-
-        // Choose action using random weighted (check min and max for negativity, use diffs?) Use sigmoid?
 
         val dsl = HelloWorldGame.game
         val gameSetup = GameSetupImpl(dsl)
-        val game = gameSetup.createGame(1, gameSetup.getDefaultConfig())
-        val agent = Agent(Random.Default)
-        while (!game.isGameOver()) {
-            val state = game.view(0)
-            val translatedView = agent.understandView(state)
-            val moveIndex = agent.decideMoveIndex(translatedView)
-            val move = game.actions.types()
-                    .sortedBy { it.name }.flatMap { it.availableActions(0) }[moveIndex]
-            performMove(game, move)
-            val nextState = game.view(0)
-            val reward = agent.observeReward(state, nextState)
-            agent.saveExperience(translatedView, moveIndex, reward)
+        val agent = LearningAgent(4)
+
+        fun <T: Any> stateToFloats(state: GameImpl<T>): FloatArray {
+            return when (val model = state.model) {
+                is HelloWorldGame.HelloWorldModel -> model.values.map { if (it) 1f else 0f }.toFloatArray()
+                else -> throw IllegalArgumentException("Unsupported type: ${state.model::class}")
+            }
         }
 
-        agent.train(3)
-        println(game.model.points)
+        val random = Random.Default
+        fun decideMoveIndex(floats: FloatArray): Int {
+            if (random.nextDouble() < 0.1) {
+                val randomAction = random.nextInt(floats.size)
+//                println("Choosing random action: $randomAction")
+                return randomAction
+            }
+            return floats.withIndex().maxBy { it.value.toDouble() }!!.index
+        }
 
+        repeat(300) {gameNumber ->
+            val game = gameSetup.createGame(1, gameSetup.getDefaultConfig())
+            var totalReward = 0f
+            var actionsMade = 0
+            val actions = mutableListOf<Int>()
+            while (!game.isGameOver()) {
+                debug("Current state: $game")
+                val stateFloats = stateToFloats(game)
+                val agentOutput = agent.runNetwork(stateFloats)
+                val moveIndex = decideMoveIndex(agentOutput)
+                actions.add(moveIndex)
+                val reward = game.performActionObserveReward(moveIndex)
+                totalReward += reward
+                val nextState = stateToFloats(game)
+                agent.saveExperience(stateFloats, moveIndex, reward, nextState, game.isGameOver())
+                if (++actionsMade % 10 == 0) {
+//                    println("$actionsMade actions made. Training time. Actions ${actions.joinToString("")}")
+                    agent.train()
+                }
+            }
+//            println("Finished game $gameNumber with $totalReward. Actions $actions")
+            println("Game $gameNumber: $totalReward points")
+            if (gameNumber % 5 == 4) {
+//                scanner.nextLine()
+            }
+        }
     }
 
-    private fun <T: Any> performMove(game: GameImpl<T>, move: Actionable<T, Any>) {
-        game.actions.type(move.actionType)!!.perform(0, move.parameter)
+    private fun <T: Any> GameImpl<T>.performActionObserveReward(moveIndex: Int): Float {
+        val playerIndex = 0
+        val move = this.actions.types()
+            .sortedBy { it.name }.flatMap { it.availableActions(playerIndex) }[moveIndex]
+        this.actions.type(move.actionType)!!.perform(playerIndex, move.parameter)
+        this.stateCheck()
+        return this.eliminationCallback.eliminations().find { it.playerIndex == playerIndex }?.winResult?.result?.toFloat() ?: 0f
     }
 
 }
@@ -122,3 +172,16 @@ class DJLReinforcement {
 fun main() {
     DJLReinforcement().play()
 }
+
+// Environments: HelloWorld, Grid-World, TTT to start with.
+// Later: Reversi, UTTT, Connect4, Quixo, TTT3D
+// Trickier: Artax (up to 4 players)
+// Trickier: UR (randomness, next player can vary)
+
+// Train using a memory of 100 or so experiences
+// Record state, action and next state(?)
+
+// Network: Input state, output Q-values for all actions
+
+
+// Choose action using random weighted (check min and max for negativity, use diffs?) Use sigmoid?
