64273200e5d550873611315c4448d62c1b3db648
Gustav Örtenberg <gustav.ortenberg@BariumAB.onmicrosoft.com>
2020-05-16 15:27:52 +0200

    added rudamentary representation of player gems and production

--- games-vue-client/src/components/games/splendor/SplendorPlayer.vue
+++ games-vue-client/src/components/games/splendor/SplendorPlayer.vue
@@ -4,11 +4,11 @@
             <p>{{ player.points }}</p>
             <p>Discounts</p>
             <div>
-                <p v-for="(value, index) in player.discounts" :key="index">{{ value }} {{ index }}</p>
+                <p v-for="(value, index) in player.discounts" :key="index" :class="'gems-' + index">{{ value }}</p>
             </div>
             <p>Money</p>
             <div>
-                <p v-for="(value, index) in player.money" :key="index">{{ value }} {{ index }}</p>
+                <span v-for="(value, index) in player.money" :key="index" :class="'gems-' + index">{{ value }}</span>
             </div>
             <p v-if="player.reserved">Reserved Cards: {{ player.reserved }}</p>
             <p v-if="player.reservedCards">Reserved</p>
@@ -22,3 +22,56 @@ export default {
     props: ["player"]
 }
 </script>
+<style>
+:root{
+    --splendor-red: #ef476f;
+    --splendor-blue: #118AB2;
+    --splendor-green: #06D6A0;
+    --splendor-black: #011627;
+    --splendor-white: #f0e6ef;
+    --splendor-yellow: #ffd166;
+}
+
+.gems-RED,
+.gems-BLUE,
+.gems-GREEN,
+.gems-BLACK,
+.gems-WHITE,
+.gems-wildcards {
+    padding: 5px 10px;
+    margin: 0px 5px;
+    border-style: solid;
+    border-width: thin;
+    border-color: var(--splendor-black) !important;
+}
+.gems-RED {
+    background-color: var(--splendor-red) !important;
+}
+.gems-BLUE {
+    background-color: var(--splendor-blue) !important;
+}
+.gems-GREEN {
+    background-color: var(--splendor-green) !important;
+}
+.gems-BLACK {
+    background-color: var(--splendor-black) !important;
+}
+.gems-WHITE {
+    background-color: var(--splendor-white) !important;
+}
+.gems-wildcards {
+    background-color: var(--splendor-yellow) !important;
+}
+
+.gems-RED,
+.gems-BLUE,
+.gems-BLACK {
+    color: var(--splendor-white) !important;
+}
+.gems-GREEN,
+.gems-WHITE,
+.gems-wildcards {
+    color: var(--splendor-black) !important;
+}
+
+</style>
