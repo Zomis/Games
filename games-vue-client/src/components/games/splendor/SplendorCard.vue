4a90ccb3f4705510b729941677df6129313cca63
Gustav Örtenberg <gustav.ortenberg@BariumAB.onmicrosoft.com>
2020-05-16 15:09:44 +0200

    Fixed a good looking card design

--- games-vue-client/src/components/games/splendor/SplendorCard.vue
+++ games-vue-client/src/components/games/splendor/SplendorCard.vue
@@ -1,8 +1,16 @@
 <template>
     <v-card :class="'discount-' + discountColor">
         <v-card-text>
-            <p>{{ card.points }}</p>
-            <p v-for="(cost, index) in card.costs" :key="index">{{ cost }} {{ index }}</p>
+            <v-row>
+                <v-col>
+                    <h1>{{ card.points }}</h1>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <span v-for="(cost, index) in card.costs" :key="index" :class="'cost-' + index">{{ cost }}</span>
+                </v-col>
+            </v-row>
         </v-card-text>
     </v-card>
 </template>
@@ -18,27 +26,73 @@ export default {
 }
 </script>
 <style>
-.discount-RED {
-    background-color: red !important;
-}
-.discount-WHITE {
-    background-color: white !important;
-}
-.discount-BLACK {
-    background-color: black !important;
-}
-.discount-GREEN {
-    background-color: green !important;
-}
-.discount-BLUE {
-    background-color: blue !important;
+:root{
+    --splendor-red: #ef476f;
+    --splendor-blue: #118AB2;
+    --splendor-green: #06D6A0;
+    --splendor-black: #011627;
+    --splendor-white: #f0e6ef;
 }
 
+.discount-RED {
+    background-color: var(--splendor-red) !important;
+}
+.discount-WHITE {
+    background-color: var(--splendor-white) !important;
+}
+.discount-BLACK {
+    background-color: var(--splendor-black) !important;
+}
+.discount-GREEN {
+    background-color: var(--splendor-green) !important;
+}
+.discount-BLUE {
+    background-color: var(--splendor-blue) !important;
+}
 .discount-RED .v-card__text,
 .discount-BLACK .v-card__text,
-.discount-BLUE .v-card__text,
-.discount-GREEN .v-card__text {
-    color: white !important;
+.discount-BLUE .v-card__text {
+    color: var(--splendor-white) !important;
 }
+
+.cost-RED,
+.cost-BLUE,
+.cost-GREEN,
+.cost-BLACK,
+.cost-WHITE {
+    padding: 5px 10px;
+    margin: 0px 5px;
+    border-style: solid;
+    border-width: thin;
+}
+.cost-RED {
+    background-color: var(--splendor-red) !important;
+}
+.cost-BLUE {
+    background-color: var(--splendor-blue) !important;
+}
+.cost-GREEN {
+    background-color: var(--splendor-green) !important;
+}
+.cost-BLACK {
+    background-color: var(--splendor-black) !important;
+}
+.cost-WHITE {
+    background-color: var(--splendor-white) !important;
+}
+
+.cost-RED,
+.cost-BLUE,
+.cost-BLACK {
+    border-color: var(--splendor-white);
+    color: var(--splendor-white) !important;
+}
+.cost-GREEN,
+.cost-WHITE {
+    border-color: var(--splendor-black);
+    color: var(--splendor-black) !important;
+}
+
+
 
 </style>
