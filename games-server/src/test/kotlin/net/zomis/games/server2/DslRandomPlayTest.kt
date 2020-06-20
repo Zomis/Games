a0f575ba38e2af08cdce0d4c2f26cf649f78edf0
Simon Forsberg <zomis2k@hotmail.com>
2020-06-21 01:10:39 +0200

    Temporary fix that works for DslRandomPlayTest not supporting ActionLog

--- games-server/src/test/kotlin/net/zomis/games/server2/DslRandomPlayTest.kt
+++ games-server/src/test/kotlin/net/zomis/games/server2/DslRandomPlayTest.kt
@@ -151,8 +151,9 @@ class DslRandomPlayTest {
             val playerSocket = players[request.player]
             val moveString = jacksonObjectMapper().writeValueAsString(request.move)
             playerSocket.send("""{ "route": "games/$dslGame/1/move", "moveType": "${request.moveType}", "move": $moveString }""")
-            p1.expectJsonObject { it.getTextOrDefault("type", "") == "GameMove" }
-            p2.expectJsonObject { it.getTextOrDefault("type", "") == "GameMove" }
+            // TODO: Also add checks for "ActionLog" messages?
+            p1.takeUntilJson { it.getTextOrDefault("type", "") == "GameMove" }
+            p2.takeUntilJson { it.getTextOrDefault("type", "") == "GameMove" }
         }
 
         // Game is finished
