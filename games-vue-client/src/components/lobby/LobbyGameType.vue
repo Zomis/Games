97ea4b2d6d70f2aa75b4ac275f23fb95d6fface7
Derrick Eriksson <derrick.eriksson@gmail.com>
2020-09-11 20:40:40 +0200

    Add players and time on Hanabi

--- games-vue-client/src/components/lobby/LobbyGameType.vue
+++ games-vue-client/src/components/lobby/LobbyGameType.vue
@@ -1,74 +1,95 @@
 <template>
-    <v-card class="game-type">
-      <v-toolbar color="cyan" dark>
-        <v-toolbar-title>{{ displayName }}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn rounded @click="createInvite(gameType)">New Game</v-btn>
-      </v-toolbar>
-      <v-card-title>
-        Users
-      </v-card-title>
-      <v-list light>
-        <template v-for="(player, index) in users">
-          <v-divider :key="`divider-${player.id}`" v-if="index > 0"></v-divider>
-          <v-list-item :key="`player-${player.id}`">
-            <v-list-item-content>
-              <v-list-item-title v-html="player.name"></v-list-item-title>
-            </v-list-item-content>
+  <v-card class="game-type">
+    <v-toolbar color="cyan" dark>
+      <v-toolbar-title>{{ displayName }}</v-toolbar-title>
+      <v-icon :color="orange">{{ mdi-account-multiple }}</v-icon>
+      {{supportedPlayers}}
+      <v-icon :color="orange">{{ mdi-clock-time-four-outline }}</v-icon>
+      {{playTime}}
+      <v-spacer></v-spacer>
+      <v-btn rounded @click="createInvite(gameType)">New Game</v-btn>
+    </v-toolbar>
+    <v-card-title>Users</v-card-title>
+    <v-list light>
+      <template v-for="(player, index) in users">
+        <v-divider :key="`divider-${player.id}`" v-if="index > 0"></v-divider>
+        <v-list-item :key="`player-${player.id}`">
+          <v-list-item-content>
+            <v-list-item-title v-html="player.name"></v-list-item-title>
+          </v-list-item-content>
 
-            <v-list-item-action>
-              <v-btn color="info" @click="invite(gameType, player.id)" v-if="player.name !== yourPlayer.name">Invite</v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </template>
-      </v-list>
+          <v-list-item-action>
+            <v-btn
+              color="info"
+              @click="invite(gameType, player.id)"
+              v-if="player.name !== yourPlayer.name"
+            >Invite</v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </template>
+    </v-list>
 
-      <v-divider></v-divider>
+    <v-divider></v-divider>
 
-      <v-card-title>Your Games</v-card-title>
-        <div v-for="game in yourGames" :key="game.gameId" class="active-game">
-            <component :key="gameType + game.gameInfo.gameId" :is="game.component" :gameInfo="game.gameInfo"></component>
-        </div>
+    <v-card-title>Your Games</v-card-title>
+    <div v-for="game in yourGames" :key="game.gameId" class="active-game">
+      <component
+        :key="gameType + game.gameInfo.gameId"
+        :is="game.component"
+        :gameInfo="game.gameInfo"
+      ></component>
+    </div>
 
-      <v-divider></v-divider>
+    <v-divider></v-divider>
 
-      <v-card-title>Other Games</v-card-title>
-        <div v-for="game in otherGames" :key="game.gameId" class="active-game">
-          <component :key="gameType + game.gameInfo.gameId" :is="game.component" :gameInfo="game.gameInfo"></component>
-        </div>
-    </v-card>
+    <v-card-title>Other Games</v-card-title>
+    <div v-for="game in otherGames" :key="game.gameId" class="active-game">
+      <component
+        :key="gameType + game.gameInfo.gameId"
+        :is="game.component"
+        :gameInfo="game.gameInfo"
+      ></component>
+    </div>
+  </v-card>
 </template>
 <script>
 import Socket from "@/socket";
 import supportedGames from "@/supportedGames";
 
 export default {
-    name: "LobbyGameType",
-    props: ["gameType", "users", "yourPlayer"],
-    methods: {
-        createInvite(gameType) {
-            Socket.route("invites/prepare", { gameType: gameType })
-        },
-        invite(gameType, playerId) {
-            Socket.route("invites/invite", { gameType: gameType, invite: [playerId] });
-        }
+  name: "LobbyGameType",
+  props: ["gameType", "users", "yourPlayer"],
+  methods: {
+    createInvite(gameType) {
+      Socket.route("invites/prepare", { gameType: gameType });
     },
-    computed: {
-        activeGames() {
-            return this.$store.getters.activeGames.filter(game => game.gameInfo.gameType === this.gameType);
-        },
-        supportedGame() {
-            return supportedGames.games[this.gameType];
-        },
-        displayName() {
-            return this.supportedGame.displayName ? this.supportedGame.displayName : this.gameType;
-        },
-        yourGames() {
-            return this.activeGames.filter(game => game.gameInfo.yourIndex >= 0)
-        },
-        otherGames() {
-            return this.activeGames.filter(game => game.gameInfo.yourIndex < 0)
-        }
+    invite(gameType, playerId) {
+      Socket.route("invites/invite", {
+        gameType: gameType,
+        invite: [playerId]
+      });
     }
-}
+  },
+  computed: {
+    activeGames() {
+      return this.$store.getters.activeGames.filter(
+        game => game.gameInfo.gameType === this.gameType
+      );
+    },
+    supportedGame() {
+      return supportedGames.games[this.gameType];
+    },
+    displayName() {
+      return this.supportedGame.displayName
+        ? this.supportedGame.displayName
+        : this.gameType;
+    },
+    yourGames() {
+      return this.activeGames.filter(game => game.gameInfo.yourIndex >= 0);
+    },
+    otherGames() {
+      return this.activeGames.filter(game => game.gameInfo.yourIndex < 0);
+    }
+  }
+};
 </script>
--- games-vue-client/src/supportedGames.js
+++ games-vue-client/src/supportedGames.js
@@ -7,7 +7,7 @@ if (typeof gamejs["games-js"] !== "undefined") {
 import PlayGame from "@/components/PlayGame";
 
 import RoyalGameOfUR from "@/components/RoyalGameOfUR";
-import DungeonMayhem from "@/components/games/dungeon-mayhem/DungeonMayhem"
+import DungeonMayhem from "@/components/games/dungeon-mayhem/DungeonMayhem";
 import UTTT from "@/components/games/UTTT";
 import Hanabi from "@/components/games/Hanabi";
 import HanabiConfig from "@/components/games/hanabi/HanabiConfig";
@@ -21,281 +21,308 @@ import LiarsDice from "@/components/games/LiarsDice";
 import Avalon from "@/components/games/Avalon";
 
 // ViewTypes for ActionLog
-import SplendorCard from "@/components/games/splendor/SplendorCard"
-import SplendorNoble from "@/components/games/splendor/SplendorNoble"
-import HanabiCard from "@/components/games/HanabiCard"
-import DungeonMayhemCard from "@/components/games/dungeon-mayhem/DungeonMayhemCard"
+import SplendorCard from "@/components/games/splendor/SplendorCard";
+import SplendorNoble from "@/components/games/splendor/SplendorNoble";
+import HanabiCard from "@/components/games/HanabiCard";
+import DungeonMayhemCard from "@/components/games/dungeon-mayhem/DungeonMayhemCard";
 
 import DslGameState from "./components/games/DslGameState";
-const defaultRouteProps = route => ({
-    gameInfo: route.params.gameInfo,
-    showRules: true
-})
+const defaultRouteProps = (route) => ({
+  gameInfo: route.params.gameInfo,
+  showRules: true,
+});
 
-const xyAction = (data) => data.x + ',' + data.y
+const xyAction = (data) => data.x + "," + data.y;
 const tttActions = {
-    play: xyAction
-}
+  play: xyAction,
+};
 const tttMoveActions = {
-    move: (source) => ({
-        key: source.x + ',' + source.y,
-        next: (target) => xyAction(target)
-    })
-}
+  move: (source) => ({
+    key: source.x + "," + source.y,
+    next: (target) => xyAction(target),
+  }),
+};
 
 const splendorActions = {
-    buy: (card) => 'buy-' + card,
-    buyReserved: (card) => 'buyReserved-' + card,
-    discardMoney: (money) => 'discardMoney-' + money,
-    takeMoney: (target1) => ({
-        key: 'take-' + target1,
-        next: (target2) => ({
-            key: 'take-' + target2,
-            next: (target3) => 'take-' + target3
-        })
+  buy: (card) => "buy-" + card,
+  buyReserved: (card) => "buyReserved-" + card,
+  discardMoney: (money) => "discardMoney-" + money,
+  takeMoney: (target1) => ({
+    key: "take-" + target1,
+    next: (target2) => ({
+      key: "take-" + target2,
+      next: (target3) => "take-" + target3,
     }),
-    reserve: (card) => 'reserve-' + card
-}
+  }),
+  reserve: (card) => "reserve-" + card,
+};
 
 const setActions = {
-    set: (target1) => ({
-        key: 'set-' + target1,
-        next: (target2) => ({
-            key: 'set-' + target2,
-            next: (target3) => 'set-' + target3
-        })
-    })
-}
+  set: (target1) => ({
+    key: "set-" + target1,
+    next: (target2) => ({
+      key: "set-" + target2,
+      next: (target3) => "set-" + target3,
+    }),
+  }),
+};
 
 function recursiveAvalon(teamMember) {
-    return {
-        key: 'players/' + teamMember,
-        next: recursiveAvalon
-    }
+  return {
+    key: "players/" + teamMember,
+    next: recursiveAvalon,
+  };
 }
 
 const supportedGames = {
-    "Avalon": {
-        dsl: gamejs.net.zomis.games.impl.ResistanceAvalonGame.game,
-        actions: {
-            teamChoice: (missionNumber) => ({
-                key: 'mission-' + missionNumber,
-                next: recursiveAvalon
-            }),
-            vote: (result) => `${result}`,
-            performMission: (result) => `${result}`,
-            assassinate: (targetPlayer) => "players/" + targetPlayer,
-            useLadyOfTheLake: (targetPlayer) => "players/" + targetPlayer
-        },
-        component: Avalon,
+  Avalon: {
+    dsl: gamejs.net.zomis.games.impl.ResistanceAvalonGame.game,
+    actions: {
+      teamChoice: (missionNumber) => ({
+        key: "mission-" + missionNumber,
+        next: recursiveAvalon,
+      }),
+      vote: (result) => `${result}`,
+      performMission: (result) => `${result}`,
+      assassinate: (targetPlayer) => "players/" + targetPlayer,
+      useLadyOfTheLake: (targetPlayer) => "players/" + targetPlayer,
     },
-    "DSL-UR": {
-        displayName: "Royal Game of UR",
-        dsl: gamejs.net.zomis.games.impl.DslUR.gameUR,
-        actions: {
-            roll: () => "roll",
-            move: (i) => `${i}`
-        },
-        component: RoyalGameOfUR,
+    component: Avalon,
+  },
+  "DSL-UR": {
+    displayName: "Royal Game of UR",
+    dsl: gamejs.net.zomis.games.impl.DslUR.gameUR,
+    actions: {
+      roll: () => "roll",
+      move: (i) => `${i}`,
     },
-    "Hanabi": {
-        dsl: gamejs.net.zomis.games.impl.HanabiGame.game,
-        configComponent: HanabiConfig,
-        actions: {
-            Play: (index) => "play-" + index,
-            Discard: (index) => "discard-" + index,
-            GiveClue: (playerIndex) => ({
-                key: 'player-' + playerIndex,
-                next: (mode) => ({
-                    key: 'giveclue-' + mode,
-                    next: (hintValue) => `giveclue-${mode}-${hintValue}`
-                })
-            })
-        },
-        viewTypes: {
-            card: { component: HanabiCard, binds: (v) => ({ card: v, doubleView: true }) }
-        },
-        component: Hanabi,
+    component: RoyalGameOfUR,
+  },
+  Hanabi: {
+    players: "2-5",
+    time: "30",
+    dsl: gamejs.net.zomis.games.impl.HanabiGame.game,
+    configComponent: HanabiConfig,
+    actions: {
+      Play: (index) => "play-" + index,
+      Discard: (index) => "discard-" + index,
+      GiveClue: (playerIndex) => ({
+        key: "player-" + playerIndex,
+        next: (mode) => ({
+          key: "giveclue-" + mode,
+          next: (hintValue) => `giveclue-${mode}-${hintValue}`,
+        }),
+      }),
     },
-    "Set": {
-        dsl: true,
-        enabled: true,
-        resetActions: false,
-        actions: setActions,
-        component: SetGame,
+    viewTypes: {
+      card: {
+        component: HanabiCard,
+        binds: (v) => ({ card: v, doubleView: true }),
+      },
     },
-    "Splendor": {
-        dsl: gamejs.net.zomis.games.impl.DslSplendor.splendorGame,
-        enabled: true,
-        actions: splendorActions,
-        component: Splendor,
-        viewTypes: {
-            card: { component: SplendorCard, binds: (v) => ({ card: v }) },
-            noble: { component: SplendorNoble, binds: (v) => ({ noble: v }) },
-        },
+    component: Hanabi,
+  },
+  Set: {
+    dsl: true,
+    enabled: true,
+    resetActions: false,
+    actions: setActions,
+    component: SetGame,
+  },
+  Splendor: {
+    dsl: gamejs.net.zomis.games.impl.DslSplendor.splendorGame,
+    enabled: true,
+    actions: splendorActions,
+    component: Splendor,
+    viewTypes: {
+      card: { component: SplendorCard, binds: (v) => ({ card: v }) },
+      noble: { component: SplendorNoble, binds: (v) => ({ noble: v }) },
     },
-    "DSL-Connect4": {
-        displayName: "Connect Four",
-        dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameConnect4,
-        actions: tttActions,
-        component: DSLTTT,
+  },
+  "DSL-Connect4": {
+    displayName: "Connect Four",
+    dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameConnect4,
+    actions: tttActions,
+    component: DSLTTT,
+  },
+  Quixo: {
+    displayName: "Quixo",
+    dsl: gamejs.net.zomis.games.impl.TTSourceDestinationGames.gameQuixo,
+    actions: tttMoveActions,
+    component: DSLTTT,
+  },
+  Skull: {
+    dsl: gamejs.net.zomis.games.impl.SkullGame.game,
+    actions: {
+      play: (index) => "hand-" + index,
+      bet: (index) => "bet-" + index,
+      discard: (index) => "hand-" + index,
+      pass: () => "pass",
+      chooseNextPlayer: (index) => "choosePlayer-" + index,
+      choose: (index) => "choose-" + index,
     },
-    "Quixo": {
-        displayName: "Quixo",
-        dsl: gamejs.net.zomis.games.impl.TTSourceDestinationGames.gameQuixo,
-        actions: tttMoveActions,
-        component: DSLTTT,
+    component: Skull,
+  },
+  LiarsDice: {
+    displayName: "Liar's Dice",
+    dsl: gamejs.net.zomis.games.impl.LiarsDiceGame.game,
+    actions: {
+      bet: (amount) => ({
+        key: "amount-" + amount,
+        next: (face) => `bet ${amount}x ${face}'s`,
+      }),
+      liar: () => "liar",
+      spotOn: () => "spotOn",
     },
-    "Skull": {
-        dsl: gamejs.net.zomis.games.impl.SkullGame.game,
-        actions: {
-            play: (index) => "hand-" + index,
-            bet: (index) => "bet-" + index,
-            discard: (index) => "hand-" + index,
-            pass: () => "pass",
-            chooseNextPlayer: (index) => "choosePlayer-" + index,
-            choose: (index) => "choose-" + index
-        },
-        component: Skull
+    component: LiarsDice,
+  },
+  "Dungeon Mayhem": {
+    dsl: gamejs.net.zomis.games.impl.DungeonMayhemDsl.game,
+    actions: {
+      play: (index) => "play-" + index,
+      target: (target) =>
+        "target:player-" +
+        target.player +
+        ";shield-" +
+        target.shieldCard +
+        ";discarded-" +
+        target.discardedCard,
     },
-    "LiarsDice": {
-        displayName: "Liar's Dice",
-        dsl: gamejs.net.zomis.games.impl.LiarsDiceGame.game,
-        actions: {
-            bet: (amount) => ({
-                key: 'amount-' + amount,
-                next: (face) => `bet ${amount}x ${face}'s`
-            }),
-            liar: () => "liar",
-            spotOn: () => "spotOn"
-        },
-        component: LiarsDice
+    viewTypes: {
+      card: {
+        component: DungeonMayhemCard,
+        binds: (v) => ({ card: v, actions: { available: {} } }),
+      },
     },
-    "Dungeon Mayhem": {
-        dsl: gamejs.net.zomis.games.impl.DungeonMayhemDsl.game,
-        actions: {
-            play: (index) => "play-" + index,
-            target: (target) => "target:player-" + target.player + ';shield-' + target.shieldCard + ';discarded-' + target.discardedCard
-        },
-        viewTypes: {
-            card: { component: DungeonMayhemCard, binds: (v) => ({ card: v, actions: { available: {} } }) }
-        },
-        component: DungeonMayhem
-    },
-    "Artax": {
-        dsl: gamejs.net.zomis.games.impl.ArtaxGame.gameArtax,
-        actions: tttMoveActions,
-        component: DSLTTT,
-    },
-    "DSL-Reversi": {
-        displayName: "Reversi",
-        dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameReversi,
-        actions: tttActions,
-        component: DSLTTT,
-    },
-    "DSL-TTT3D": {
-        displayName: "3D Tic-Tac-Toe / Connect Four",
-        dsl: gamejs.net.zomis.games.impl.ttt.TTT3DGame.game,
-        actions: tttActions,
-        component: TTT3D,
-    },
-    "DSL-UTTT": {
-        displayName: "Tic-Tac-Toe Ultimate",
-        dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameUTTT,
-        actions: tttActions,
-        component: UTTT,
-    },
-    "DSL-TTT": {
-        displayName: "Tic-Tac-Toe",
-        dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.game,
-        actions: tttActions,
-        component: DSLTTT,
-    }
-}
+    component: DungeonMayhem,
+  },
+  Artax: {
+    dsl: gamejs.net.zomis.games.impl.ArtaxGame.gameArtax,
+    actions: tttMoveActions,
+    component: DSLTTT,
+  },
+  "DSL-Reversi": {
+    displayName: "Reversi",
+    dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameReversi,
+    actions: tttActions,
+    component: DSLTTT,
+  },
+  "DSL-TTT3D": {
+    displayName: "3D Tic-Tac-Toe / Connect Four",
+    dsl: gamejs.net.zomis.games.impl.ttt.TTT3DGame.game,
+    actions: tttActions,
+    component: TTT3D,
+  },
+  "DSL-UTTT": {
+    displayName: "Tic-Tac-Toe Ultimate",
+    dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.gameUTTT,
+    actions: tttActions,
+    component: UTTT,
+  },
+  "DSL-TTT": {
+    displayName: "Tic-Tac-Toe",
+    dsl: gamejs.net.zomis.games.impl.ttt.DslTTT.game,
+    actions: tttActions,
+    component: DSLTTT,
+  },
+};
 
 function enabledGames() {
-    return Object.keys(supportedGames).filter(key => supportedGames[key].enabled !== false)
+  return Object.keys(supportedGames).filter(
+    (key) => supportedGames[key].enabled !== false
+  );
 }
 
 export default {
-    gamejs: gamejs,
-    displayName(gameType) {
-        let game = supportedGames[gameType]
-        return game && game.displayName ? game.displayName : gameType
-    },
-    storeModules() {
-        let modules = {}
-        for (let key of enabledGames()) {
-            let game = supportedGames[key]
-            if (game.dsl) {
-                modules["DslGameState"] = DslGameState
-            } else if (game.store) {
-                modules[key] = game.store
-            }
-        }
-        return modules
-    },
-    routes() {
-        return enabledGames().map(key => {
-            let game = supportedGames[key]
-            let routeProps = game.routeProps || defaultRouteProps
-            return {
-                path: `/games/${key}/:gameId`,
-                name: game.routeName || key,
-                component: PlayGame,
-                props: route => ({
-                    gameType: key,
-                    gameId: route.params.gameId,
-                    ...routeProps(route)
-                })
-            }
-        })
-    },
-    components() {
-        let components = {}
-        for (let key of enabledGames()) {
-            let game = supportedGames[key]
-            let componentName = game.component.name
-            components[componentName] = game.component
-        }
-        return components
-    },
-    resolveActionKey(supportedGame, actionData, actionChoice) {
-        let actionName = actionData.actionType
-        let value = actionData.serialized
-        let actionKeys = supportedGame.actions[actionName]
-        if (!actionKeys) {
-            console.log("No actionKeys set for", actionName, "default to same as serialized value", value)
-            return value
-        }
-        if (actionChoice === null) {
-            actionChoice = { choices: [] };
-        }
-        let choices = actionChoice.choices.slice()
-        let a = actionKeys;
-        for (let i = 0; i < choices.length; i++) {
-            a = a(choices[i]); // Returns either string or { key: string, next: function }
-            if (typeof a === 'object' && a.next) {
-                a = a.next
-            }
-        }
-        if (typeof a !== 'function') {
-            console.error("'a' is not a function", a, actionName, actionData, actionChoice)
-            //return a;
-            throw a;
-        }
-        a = a(value)
-        if (typeof a === 'object') {
-            return a.key
-        }
-        return a
-    },
-    stateModules(state) {
-        let modules = this.storeModules()
-        return Object.keys(modules).map(moduleName => state[moduleName]);
-    },
-    enabledGameKeys() {
-        return enabledGames()
-    },
-    games: supportedGames
-}
+  gamejs: gamejs,
+  displayName(gameType) {
+    let game = supportedGames[gameType];
+    return game && game.displayName ? game.displayName : gameType;
+  },
+  storeModules() {
+    let modules = {};
+    for (let key of enabledGames()) {
+      let game = supportedGames[key];
+      if (game.dsl) {
+        modules["DslGameState"] = DslGameState;
+      } else if (game.store) {
+        modules[key] = game.store;
+      }
+    }
+    return modules;
+  },
+  routes() {
+    return enabledGames().map((key) => {
+      let game = supportedGames[key];
+      let routeProps = game.routeProps || defaultRouteProps;
+      return {
+        path: `/games/${key}/:gameId`,
+        name: game.routeName || key,
+        component: PlayGame,
+        props: (route) => ({
+          gameType: key,
+          gameId: route.params.gameId,
+          ...routeProps(route),
+        }),
+      };
+    });
+  },
+  components() {
+    let components = {};
+    for (let key of enabledGames()) {
+      let game = supportedGames[key];
+      let componentName = game.component.name;
+      components[componentName] = game.component;
+    }
+    return components;
+  },
+  resolveActionKey(supportedGame, actionData, actionChoice) {
+    let actionName = actionData.actionType;
+    let value = actionData.serialized;
+    let actionKeys = supportedGame.actions[actionName];
+    if (!actionKeys) {
+      console.log(
+        "No actionKeys set for",
+        actionName,
+        "default to same as serialized value",
+        value
+      );
+      return value;
+    }
+    if (actionChoice === null) {
+      actionChoice = { choices: [] };
+    }
+    let choices = actionChoice.choices.slice();
+    let a = actionKeys;
+    for (let i = 0; i < choices.length; i++) {
+      a = a(choices[i]); // Returns either string or { key: string, next: function }
+      if (typeof a === "object" && a.next) {
+        a = a.next;
+      }
+    }
+    if (typeof a !== "function") {
+      console.error(
+        "'a' is not a function",
+        a,
+        actionName,
+        actionData,
+        actionChoice
+      );
+      //return a;
+      throw a;
+    }
+    a = a(value);
+    if (typeof a === "object") {
+      return a.key;
+    }
+    return a;
+  },
+  stateModules(state) {
+    let modules = this.storeModules();
+    return Object.keys(modules).map((moduleName) => state[moduleName]);
+  },
+  enabledGameKeys() {
+    return enabledGames();
+  },
+  games: supportedGames,
+};
