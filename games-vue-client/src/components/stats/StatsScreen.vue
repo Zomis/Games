3576e9595e287b66eb5994e4c584d66078e77a02
Robin Larsson <robin@larssonweb.se>
2020-10-31 22:52:54 +0100

    Run query on load, update url when applying filters, cleanup.

--- games-vue-client/src/components/stats/StatsScreen.vue
+++ games-vue-client/src/components/stats/StatsScreen.vue
@@ -5,11 +5,11 @@
                 <v-text-field v-model="playerConditions" label="Player Ids" />
             </v-col>
             <v-col cols="2">
-                <v-select :items="supportedGames" item-text="text" item-value="value" v-model="tagConditions">
+                <v-select :items="supportedGames" item-text="text" item-value="value" v-model="tagConditions" label="Game">
                 </v-select>
             </v-col>
             <v-col cols="2">
-                <v-btn @click="query()">Apply filter</v-btn>
+                <v-btn @click="applyFilter()">Apply filter</v-btn>
             </v-col>
         </v-row>
         <v-row dense v-for="game in gameList" :key="game.gameId">
@@ -80,32 +80,39 @@ const appendedData = (data) => data.map((game) => {
     return { ...game, title: buildTitleFromGame(game) };
 });
 
-const mockedData = [{"gameId":"007748a7-c4a8-443f-9b90-ff1819d42174","players":[{"playerId":"b4c6bb4b-3430-4384-9670-3b6e80f7ee17","result":10,"resultPosition":1},{"playerId":"29dac597-f2b2-4378-80dc-b31c2d3edb2d","result":1,"resultPosition":4},{"playerId":"29dac597-f2b2-4378-80dc-b31c2d3edb2d","result":12,"resultPosition":3},{"playerId":"c4ec6785-c78f-4bba-9133-5f3dfe35c369","result":12,"resultPosition":2}],"tags":[{"tagId":"type/Artax","tagParameter":1585233148}]},{"gameId":"098619fe-9b97-4d47-88df-de9d005511ad","players":[{"playerId":"3a4d4cfa-9dc3-4701-a971-d21036abf127","result":14,"resultPosition":2},{"playerId":"b4c6bb4b-3430-4384-9670-3b6e80f7ee17","result":15,"resultPosition":2}],"tags":[{"tagId":"type/Hanabi","tagParameter":1588378068}]},{"gameId":"0b14b4f5-651a-4366-a3de-c902242470e3","players":[{"playerId":"b4c6bb4b-3430-4384-9670-3b6e80f7ee17","result":12,"resultPosition":5},{"playerId":"c63307d1-d284-4f98-96f7-e15cd8e95e48","result":12,"resultPosition":5},{"playerId":"6fd16ddd-8386-4c24-883c-0ae833effbdd","result":11,"resultPosition":5},{"playerId":"39c79b22-0247-4b49-a58d-e747bf4c0d9a","result":3,"resultPosition":5},{"playerId":"1cc08b1c-e904-4128-ba34-40fd9f17b087","result":2,"resultPosition":5}],"tags":[{"tagId":"type/Hanabi","tagParameter":1588793132},{"tagId":"type/Artax","tagParameter":1585233148}]}];
-
 export default {
     name: "StatsScreen",
     props: ["players", "tags"],
     data() {
         return {
-            supportedGames: supportedGames.enabledGamesTextValue(),
-            gameList: appendedData(mockedData), // run query onload?
-            playerConditions: this.players ? this.players : "", // TODO update url when filter is applied?
+            supportedGames: [
+                { text: '', value: '' },
+                ...supportedGames.enabledGamesTextValue()
+            ],
+            gameList: [],
+            playerConditions: this.players ? this.players : "",
             tagConditions: this.tags ? this.tags : "",
             chosenTag: "",
         }
     },
+    created() {
+        this.query();
+    },
     methods: {
+        applyFilter() {
+            this.$router.push(`/stats?players=${this.playerConditions}&tags=${this.tagConditions}`);
+            this.query();
+        },
         query() {
-            let cleanURI = encodeURI(`${this.baseURL}stats/query?players=${this.playerConditions}&tags=${this.tagConditions}`).replace(/#/g, '%23')
+            const cleanURI = encodeURI(`${this.baseURL}stats/query?players=${this.playerConditions}&tags=${this.tagConditions}`).replace(/#/g, '%23')
             axios.get(cleanURI).then(response => {
-                console.log(response.data);
                 this.gameList = appendedData(response.data);
             })
         }
     },
     computed: {
         baseURL() {
-            return 'https://games.zomis.net:42638/' // process.env.VUE_APP_URL
+            return process.env.VUE_APP_URL
         }
     }
 }
