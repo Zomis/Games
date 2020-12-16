47292c55d35ca6ed3c0b45218ed05dbdaf038e2c
Gustav Örtenberg <gustav.ortenberg@gmail.com>
2020-12-16 23:48:29 +0100

    Started on theory board

---
+++ games-core/src/main/kotlin/net/zomis/games/impl/alchemists/TheoryBoard.kt
@@ -0,0 +1,52 @@
+package net.zomis.games.impl.alchemists
+
+object TheoryBoard {
+    val theories = listOf(Alchemists.Ingredient.values().map { Theory(mutableListOf(), it, null) })
+}
+
+class Theory(
+        val seals: MutableList<Seal>,
+        val ingredient: Alchemists.Ingredient,
+        var alchemical: AlchemistsChemical?
+) {
+
+    fun getOwnersOfNotProperlyHedgedSeals(solution: AlchemistsChemical): List<AlchemistsModel.Player> {
+        val diffs = diff(solution, this.alchemical!!)
+        return seals.filter { !it.properlyHedged(diffs) }.map { it.owner }
+    }
+
+    private fun diff(x: AlchemistsChemical, y: AlchemistsChemical): List<AlchemistsColor> {
+        return AlchemistsColor.values().filter { x.properties.getValue(it).sign != y.properties.getValue(it).sign }
+    }
+
+}
+
+data class Seal(val hedge: AlchemistsColor?, val victoryPoints: Int, val owner: AlchemistsModel.Player) {
+
+    fun properlyHedged(differences: List<AlchemistsColor>): Boolean {
+        return when (differences.count()) {
+            0 -> true
+            1 -> when (hedge) {
+                null -> false
+                else -> differences.first() == hedge
+            }
+            else -> false
+        }
+    }
+}
+
+fun getPlayersSeals(player: AlchemistsModel.Player): List<Seal> {
+    return listOf(
+            Seal(null, 5, player),
+            Seal(null, 5, player),
+            Seal(null, 3, player),
+            Seal(null, 3, player),
+            Seal(null, 3, player),
+            Seal(AlchemistsColor.BLUE, 0, player),
+            Seal(AlchemistsColor.BLUE, 0, player),
+            Seal(AlchemistsColor.GREEN, 0, player),
+            Seal(AlchemistsColor.GREEN, 0, player),
+            Seal(AlchemistsColor.RED, 0, player),
+            Seal(AlchemistsColor.RED, 0, player)
+    )
+}
