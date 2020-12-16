9e7711c5d64d68a0feb23ab86ec2e79f52ecd41c
Gustav Örtenberg <gustav.ortenberg@gmail.com>
2020-12-16 18:56:22 +0100

    Add artifact interface

---
+++ games-core/src/main/kotlin/net/zomis/games/impl/alchemists/Artifacts/Artifact.kt
@@ -0,0 +1,11 @@
+package net.zomis.games.impl.alchemists.Artifacts
+
+interface Artifact {
+
+    val name: String
+    val description: String
+    val level: Int
+    val cost: Int
+    val points: Int
+
+}
