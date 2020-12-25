4fe314bc54be67ad7bb91d1396b482589c3ea65f
Simon Forsberg <zomis2k@hotmail.com>
2020-12-25 22:33:26 +0100

    Fix AWS deployment building Dockerfile correctly

--- games-server/.codedeploy/start_server.sh
+++ games-server/.codedeploy/start_server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# This assumes there is a server2.conf file that specifies `-wsPort 42638`
-docker build -t server2 .
-docker run -d --rm --name games_server -p 42638:42638 -v /home/ec2-user/server2:/data/logs -w /data/logs server2
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
+pwd
+docker images -q --filter reference="server2*" | xargs -r docker rmi
+docker build -t server2 -f "./Dockerfile" .
+# This assumes there is a server2.conf file that specifies `-wsPort 42638` 
+docker run -d --rm --name games_server -p 42638:42638 -v /home/ec2-user/server2:/data/logs -v /etc/letsencrypt:/etc/letsencrypt -w /data/logs server2
