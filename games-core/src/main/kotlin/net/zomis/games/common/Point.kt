184e4ab04f40bcc6776934547c7fafdf987cf2ba
Simon Forsberg <zomis2k@hotmail.com>
2020-12-29 23:38:20 +0100

    Improve Point class with operators and Manhattan distance

--- games-core/src/main/kotlin/net/zomis/games/common/Point.kt
+++ games-core/src/main/kotlin/net/zomis/games/common/Point.kt
@@ -4,10 +4,10 @@ import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
 data class Point(val x: Int, val y: Int) {
-    fun minus(other: Point): Point {
-        return Point(x - other.x, y - other.y)
-    }
+    operator fun minus(other: Point): Point = Point(x - other.x, y - other.y)
+    operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
 
+    fun manhattanDistance(): Int = kotlin.math.abs(x) + kotlin.math.abs(y)
     fun abs(): Point = Point(this.x.absoluteValue, this.y.absoluteValue)
     fun distance(): Double = sqrt(this.x.toDouble() * this.x + this.y.toDouble() * this.y)
 }
