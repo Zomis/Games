9d2a350db18aba8d9eb6bef7d879755844cd68ac
Robin Larsson <robin@larssonweb.se>
2021-03-28 14:20:49 +0200

    Add coup. Refactor.

--- games-vue-client/snapshots.js
+++ games-vue-client/snapshots.js
@@ -1,28 +1,18 @@
 const PercyScript = require('@percy/script');
 
 const BASE_URL = 'http://localhost:8080';
-const AUTH_COOKIE = 'cookie:cookie:C3qH3VVmoqR/rbxJIiVys1jW5jE0bDtuixWVorDfJhE='
+const AUTH_COOKIE = 'C3qH3VVmoqR/rbxJIiVys1jW5jE0bDtuixWVorDfJhE='
 
 PercyScript.run(async (page, percySnapshot) => {
     try {
-        await page.evaluateOnNewDocument(
-            token => {
-                localStorage.clear();
-                localStorage.setItem('authCookie', token);
-                localStorage.setItem('lastUsedProvider', 'guest');
-                localStorage.setItem('cookie:accepted', 'true');
-            }, AUTH_COOKIE);
-
-        await page.goto(BASE_URL, { waitUntil: 'load' });
-
-        const snap = camera(page, percySnapshot);
-
+        await login(page);
         await page.waitForSelector('.game-type');
-        await percySnapshot('homepage');
 
-        await snap('spice-road');
-        await goBackToGameList(page);
-        await snap('dixit');
+        const snapshotGame = gameCamera(page, percySnapshot);
+        await percySnapshot('homepage');
+        await snapshotGame('spice-road', '.list-complete-item', 15);
+        await snapshotGame('dixit', '.v-image__image', 7);
+        await snapshotGame('coup', '.actions-table', 1);
 
         await page.close();
         process.exit(0);
@@ -31,15 +21,32 @@ PercyScript.run(async (page, percySnapshot) => {
     }
 });
 
-const camera = (page, percySnapshot) => async (game) => {
+const gameCamera = (page, percySnapshot) => async (game, selector, expectedItems) => {
     await page.click(`.game-${game} .v-btn`);
     await page.waitForSelector('.game');
     await page.waitForSelector('.animation-list-complete');
-    // TODO: add extra waitFor to make sure the game has loaded. Perhaps check on some status instead.
+
+    if (selector !== undefined) {
+        await page.waitForFunction(`document.querySelectorAll("${selector}") && document.querySelectorAll("${selector}").length === ${expectedItems}`);
+    }
+
     await percySnapshot(game);
+    await goBackToGameList(page);
 }
 
 const goBackToGameList = async (page) => {
     await page.click('.router-link-active');
     await page.waitForSelector('.game-type');
+}
+
+const login = async (page) => {
+    await page.evaluateOnNewDocument(
+        token => {
+            localStorage.clear();
+            localStorage.setItem('authCookie', token);
+            localStorage.setItem('lastUsedProvider', 'guest');
+            localStorage.setItem('cookie:accepted', 'true');
+        }, AUTH_COOKIE);
+
+    await page.goto(BASE_URL, { waitUntil: 'load' });
 }
