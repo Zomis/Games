42ad5f8c5221724d5c0da8b12ee07f8c9816cd08
Simon Forsberg <zomis2k@hotmail.com>
2021-04-05 00:54:14 +0200

    Add List<T>.shifted function

--- games-core/src/main/kotlin/net/zomis/games/common/GameLists.kt
+++ games-core/src/main/kotlin/net/zomis/games/common/GameLists.kt
@@ -1,3 +1,8 @@
 package net.zomis.games.common
 
 fun <T: Any> T.toSingleList(): List<T> = listOf(this)
+fun <T> List<T>.shifted(steps: Int): List<T> {
+    require(steps >= 0)
+    require(steps < this.size)
+    return this.subList(steps, this.size) + this.subList(0, steps)
+}
