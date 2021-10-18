6df04ffee505ed3ba7c00b4f39649f9166744c84
Simon Forsberg <zomis2k@hotmail.com>
2021-10-18 21:56:35 +0200

    Improve Point class

--- games-core/src/main/kotlin/net/zomis/games/common/Point.kt
+++ games-core/src/main/kotlin/net/zomis/games/common/Point.kt
@@ -1,15 +1,15 @@
 package net.zomis.games.common
 
+import kotlin.math.abs
 import kotlin.math.absoluteValue
 import kotlin.math.sqrt
 
 data class Point(val x: Int, val y: Int) {
-    fun minus(other: Point): Point {
-        return Point(x - other.x, y - other.y)
-    }
-
     fun abs(): Point = Point(this.x.absoluteValue, this.y.absoluteValue)
     fun distance(): Double = sqrt(this.x.toDouble() * this.x + this.y.toDouble() * this.y)
+    fun manhattanDistance(other: Point): Int = abs(x - other.x) + abs(y - other.y)
+    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    operator fun minus(other: Point): Point = Point(x - other.x, y - other.y)
 }
 
 data class PointMove(val source: Point, val destination: Point)
