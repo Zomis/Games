088c1946504754477b0b6f42e0e0de566b9e45bb
Simon Forsberg <zomis2k@hotmail.com>
2021-12-14 16:49:35 +0100

    Update Log4j version, again
    You can never be too safe these days

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,8 +72,8 @@ kotlin {
 
                 implementation("com.beust:jcommander:1.81")
                 implementation("org.java-websocket:Java-WebSocket:1.5.2")
-                implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.15.0")
-                implementation("org.apache.logging.log4j:log4j-core:2.15.0")
+                implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.16.0")
+                implementation("org.apache.logging.log4j:log4j-core:2.16.0")
 
                 implementation("io.javalin:javalin:2.8.0")
                 implementation("com.github.kittinunf.fuel:fuel:2.3.1")
