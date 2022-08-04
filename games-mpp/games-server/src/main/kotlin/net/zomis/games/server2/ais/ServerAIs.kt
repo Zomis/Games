672c80dfbfa07cbf391b53718996b60d3e67cd46
Simon Forsberg <zomis2k@hotmail.com>
2022-08-04 16:04:49 +0200

    Null safety check for random action

--- games-mpp/games-server/src/main/kotlin/net/zomis/games/server2/ais/ServerAIs.kt
+++ games-mpp/games-server/src/main/kotlin/net/zomis/games/server2/ais/ServerAIs.kt
@@ -8,7 +8,7 @@ import net.zomis.games.server2.games.ServerGame
 object ServerAIs {
 
     fun randomAction(game: ServerGame, client: Client, index: Int): PlayerGameMoveRequest? {
-        val controller = game.obj!!
+        val controller = game.obj ?: return null
         val actionable = GameAIs.randomActionable(controller, index)
         return actionable?.let {
             PlayerGameMoveRequest(client, game, it.playerIndex, it.actionType, it.parameter, false)
