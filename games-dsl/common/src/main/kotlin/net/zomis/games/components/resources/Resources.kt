fac56a9955bc63e9bc73034382df52d1a243f4b1
Simon Forsberg <zomis2k@hotmail.com>
2022-12-01 00:50:12 +0100

    Add names to GameResource, and a simple implementation with name

--- games-dsl/common/src/main/kotlin/net/zomis/games/components/resources/Resources.kt
+++ games-dsl/common/src/main/kotlin/net/zomis/games/components/resources/Resources.kt
@@ -1,10 +1,18 @@
 package net.zomis.games.components.resources
 
 interface GameResource {
+    val name: String
     fun defaultValue(): Int = 0
     fun toResourceMap(value: Int = 1): ResourceMap = ResourceMapImpl(
         mutableMapOf(this to ResourceEntryImpl(this, value))
     )
     operator fun plus(other: GameResource): ResourceMap = this.toResourceMap().plus(other.toResourceMap())
     operator fun times(value: Int): ResourceMap = this.toResourceMap(value)
+    companion object {
+        fun withName(name: String): GameResource = SimpleGameResource(name)
+    }
+}
+
+class SimpleGameResource(override val name: String): GameResource {
+    override fun toString(): String = name
 }
