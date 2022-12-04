dc9edd0b7c01b786dc7519a14f5acd954e789937
Simon Forsberg <zomis2k@hotmail.com>
2022-12-04 21:39:48 +0100

    Downgrade Kotlin because of Compose (again?)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10") // Upgrading may cause incompatibility with Compose compiler
         // classpath("com.android.tools.build:gradle:7.0.4")
     }
 }
