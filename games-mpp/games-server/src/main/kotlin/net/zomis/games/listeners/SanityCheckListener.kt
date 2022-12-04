5895e947dcee533041b5718ee28a293453fcc964
Simon Forsberg <zomis2k@hotmail.com>
2022-12-04 21:00:21 +0100

    Sanity Check: Ignore all "/id" paths, and Grizzled actions
    Ignoring all */id paths to allow cards having an id used for frontend. Grizzled actions view also contain card ids so ignoring those as well.

--- games-mpp/games-server/src/main/kotlin/net/zomis/games/listeners/SanityCheckListener.kt
+++ games-mpp/games-server/src/main/kotlin/net/zomis/games/listeners/SanityCheckListener.kt
@@ -68,13 +68,13 @@ class SanityCheckListener(val game: Game<out Any>): GameListener {
     }
 
     private val ignorePaths = listOf(
-        Regex("^/Hanabi/[^/]+/(others/\\d+|hand)/cards/\\d+/id$"),
+        Regex("^/Grizzled/[^/]+/actions/.*"),
         Regex("^/Hanabi/[^/]+/lastAction/.*"),
-        Regex("^/Hanabi/[^/]+/colors/\\d+/(board|discard)/\\d+/id$"),
         Regex("^/Dixit/[^/]+/board$")
     )
 
     private fun viewMatch(path: String, a: Any?, b: Any?, viewer: PlayerIndex): Boolean {
+        if (path.endsWith("/id")) return true // Ignore ids for cards etc.
         if (ignorePaths.any { it.matches(path) }) {
             println("Ignoring $path: $a vs. $b")
             return true
