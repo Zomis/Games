6e536e8872c56a4764e54a56effce2f1d7d0e198
Simon Forsberg <zomis2k@hotmail.com>
2023-05-23 21:13:27 +0200

    Compose: Fix main class in jar manifest

--- games-compose/common/build.gradle.kts
+++ games-compose/common/build.gradle.kts
@@ -78,7 +78,7 @@ kotlin {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "net.zomis.games.compose.MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "jvm"
