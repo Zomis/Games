8a007b0120c87cac8f2781e2bdac2f49ad919695
Simon Forsberg <zomis2k@hotmail.com>
2024-02-24 20:41:16 +0100

    Fix List<T>.shifted function to support any steps by using fmod

--- games-dsl/common/src/main/kotlin/net/zomis/games/components/GameLists.kt
+++ games-dsl/common/src/main/kotlin/net/zomis/games/components/GameLists.kt
@@ -2,9 +2,10 @@ package net.zomis.games.common
 
 fun <T: Any> T.toSingleList(): List<T> = listOf(this)
 fun <T> List<T>.shifted(steps: Int): List<T> {
-    require(steps >= 0)
-    require(steps < this.size)
-    return this.subList(steps, this.size) + this.subList(0, steps)
+    val actualSteps = steps.fmod(size)
+    require(actualSteps >= 0)
+    require(actualSteps < this.size)
+    return this.subList(actualSteps, this.size) + this.subList(0, actualSteps)
 }
 operator fun <T> List<T>.times(multiplier: Int): List<T> = (1 until multiplier)
     .fold(this.toList()) { acc, _ -> acc + this.toList() }
